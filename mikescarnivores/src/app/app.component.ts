27887364f3a639c60e2d52edaaab5cf4ed86d552
Mike Helfrich <helfrichmichael@users.noreply.github.com>
2024-03-21 18:20:39 -0400

    CHANGE: Begin componentizing the project and update the favicon/logo.

--- mikescarnivores/src/app/app.component.ts
+++ mikescarnivores/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainLinksComponent } from './main-links/main-links.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainLinksComponent, HeaderComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ mikescarnivores/src/app/main-links/main-links.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-main-links',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './main-links.component.html',
+  styleUrl: './main-links.component.scss'
+})
+export class MainLinksComponent {
+
+}
