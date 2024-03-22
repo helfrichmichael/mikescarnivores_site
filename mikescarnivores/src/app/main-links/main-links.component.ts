2bae9679a181dc2b0b41b33a80711aedeb2c98cd
Mike Helfrich <helfrichmichael@users.noreply.github.com>
2024-03-21 20:31:39 -0400

    CHANGE: Add main links.

--- mikescarnivores/src/app/main-links/main-links.component.ts
+++ mikescarnivores/src/app/main-links/main-links.component.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+
+declare interface Link {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-main-links',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './main-links.component.html',
   styleUrl: './main-links.component.scss'
 })
 export class MainLinksComponent {
-
+  links: Link[] = [
+    {
+      name: 'Instagram',
+      url: 'https://instagram.com/mikescarnivores'
+    },
+    {
+      name: 'Shop (Order Plants)',
+      url: 'https://shop.mikescarnivores.com/',
+    },
+    {
+      name: '3D Prints for Sale',
+      url: 'https://mikescarnivores.com/3d',
+    },
+    {
+      name: 'Buy me a Coffee',
+      url: 'https://www.buymeacoffee.com/helfrichmichael',
+    }
+  ];
 }
